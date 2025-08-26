e011a04b62e1e78d5ceffebc2a9d7f6b2b5623f4
Edii4 <edvardkollar@gmail.com>
2025-08-26 16:55:28 +0200

    Starting the project

---
+++ src/main/java/dev/edi/socialmedia/SocialmediaApplication.java
@@ -0,0 +1,16 @@
+package dev.edi.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SocialmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaApplication.class, args);
+	}
+
+}
